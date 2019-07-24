b16dac5c8045730dedf96ea13347d99fe6b2e38f
Trygve Lie <post@trygve-lie.com>
2019-07-24 20:07:57 +0200

    Fix tests

--- tap-snapshots/test-plugin.mjs-TAP.test.js
+++ tap-snapshots/test-plugin.mjs-TAP.test.js
@@ -91,6 +91,7 @@ class Inner extends LitElement {
     static get styles() {
         return [css\`:host { color: red; }\`];
     }
+
     render(world) {
         return html\`<p>Hello \${world}!</p>\`;
     }
